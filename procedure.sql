cac917ff3c5d88c753acc399aa6695e87eecc51c
Simon Caceres <simon.caceres@egt.cl>
2021-05-31 20:39:50 -0400

    procedure

---
+++ procedure.sql
@@ -0,0 +1,9 @@
+DELIMITER //
+
+CREATE PROCEDURE prestamo(usuario_id INT, libro_id INT)
+BEGIN
+    INSERT INTO libros_usuarios(libro_id, usuario_id) VALUES(libro_id, usuario_id);
+    UPDATE libros SET stock = stock - 1 WHERE libros.libro_id = libro_id;
+END //
+
+DELIMITER ; 
